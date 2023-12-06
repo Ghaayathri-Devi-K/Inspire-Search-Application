e24b135d6fa7c957984babe272a40893543224e8
Ghaayathri Devi K <99457944+Ghaayathri-Devi-K@users.noreply.github.com>
2023-12-06 14:58:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+from constants import openai_key
+from langchain.llms import OpenAI
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import SequentialChain
+import streamlit as st
+
+## Integrate our code OpenAI API
+os.environ["OPENAI_API_KEY"]=openai_key
+
+# streamlit framework
+st.title('Get to know your inspiration!')
+input_text=st.text_input("Search the person whose life inspires you")
+
+# Prompt Templates
+first_input_prompt=PromptTemplate(
+    input_variables=['name'],
+    template="Tell me about the life of the {name}"
+)
+
+# Memory
+person_memory = ConversationBufferMemory(input_key='name', memory_key='chat_history')
+dob_memory = ConversationBufferMemory(input_key='person', memory_key='chat_history')
+descr_memory = ConversationBufferMemory(input_key='dob', memory_key='description_history')
+
+## OPENAI LLMS
+llm=OpenAI(temperature=0.8)
+chain=LLMChain(
+    llm=llm,prompt=first_input_prompt,verbose=True,output_key='person',memory=person_memory)
+
+# Prompt Templates
+second_input_prompt=PromptTemplate(
+    input_variables=['person'],
+    template="when was {person} born"
+)
+## OPENAI LLMS
+chain2=LLMChain(
+    llm=llm,prompt=second_input_prompt,verbose=True,output_key='dob',memory=dob_memory)
+
+# Prompt Templates
+third_input_prompt=PromptTemplate(
+    input_variables=['dob'],
+    template="Mention 5 major events happened around {dob} in the world"
+)
+## OPENAI LLMS
+chain3=LLMChain(llm=llm,prompt=third_input_prompt,verbose=True,output_key='description',memory=descr_memory)
+
+## Parent Chain
+parent_chain=SequentialChain(
+    chains=[chain,chain2,chain3],input_variables=['name'],output_variables=['person','dob','description'],verbose=True)
+
+
+
+if input_text:
+    st.write(parent_chain({'name':input_text}))
+
+    with st.expander('Person Name'): 
+        st.info(person_memory.buffer)
+
+    with st.expander('Major Events'): 
+        st.info(descr_memory.buffer)
